7bb8bcb20923fbc7901eea765654ba6aae2fbe76
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2025-03-27 01:45:41 +0530

    added search bar fucntioanalities and UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import {fetchMovies} from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { Link, useRouter } from "expo-router";
-import { Image, ScrollView, StatusBar, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, StatusBar, Text, View } from "react-native";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const {
+    data: movies, 
+    error,
+    loading
+  } = useFetch(()=>fetchMovies({
+    query:''
+  }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -14,18 +25,47 @@ export default function Index() {
         barStyle="light-content"
         backgroundColor="#030014"
       />
-      <Image source={images.bg} className="absolute w-full z-0"></Image>
+      <Image source={images.bg} className="absolute w-full z-0" resizeMode='cover'/>
 
 
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom:10}}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-        <View className="flex mt-5">
-          <SearchBar
-            onPress={()=>router.push("/search")}
-            placeholder="Search for a movie"
-          />
 
-        </View>
+        {loading
+          ?(
+            <ActivityIndicator size='large' color='#0000ff' className="mt-10 self-center"/>
+          )
+          :error
+          ?<Text>{error.message}</Text>
+          :(
+            <View className="flex mt-5">
+              <SearchBar
+                onPress={()=>router.push("/search")}
+                placeholder="Search for a movie"
+              />
+              <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+              <FlatList 
+                  data={movies}
+                  renderItem={({item})=>(
+                      <MovieCard {...item}/>
+                  )}
+                  keyExtractor={(item)=>item.id.toString()}
+                  numColumns={3}
+                  columnWrapperStyle={{
+                    justifyContent:'flex-start',
+                    gap:20,
+                    paddingRight:5,
+                    marginBottom:10
+                  }}
+                  className="mt-2 pb-32"
+                  scrollEnabled={false}
+                >
+              </FlatList>
+              </> 
+            </View>
+          )
+        }
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -2,22 +2,24 @@ import axios from 'axios'
 
 export  const API_CONFIG = {
     BASE_URL : "https://api.themoviedb.org/3",
-    Access_token: process.env.Read_Access_Token,
+    Access_token: process.env.EXPO_PUBLIC_Read_Access_Token,
     Headers:{
         accept:'application/json',
-        Authorization: `Bearer ${process.env.Read_Access_Token}`
-    }
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_Read_Access_Token}`
+    } 
 }
 
 export const fetchMovies = async ({query}:{query:string}) =>{
     const endpoints = query 
     ? `${API_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     :`${API_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
-
+    
     try{
-        const result = await axios.get('endpoints',{headers:API_CONFIG.Headers});
-        return result.data;
-    }catch(err:any){
-        throw new Error('Failed to fetch movies', err.message)
+        const response = await axios.get(endpoints,{headers:API_CONFIG.Headers});
+        return response.data.results;
+    }catch(error:any){
+        throw new Error('Failed to fetch movies', error)
     }
-}
+}   
+                
+export default fetchMovies; 
