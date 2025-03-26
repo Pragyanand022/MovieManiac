4e6ed99cec7fcb33ea9a0138a1895c0b42f422ca
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2025-03-26 19:45:06 +0530

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,32 @@
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Link, useRouter } from "expo-router";
+import { Image, ScrollView, StatusBar, Text, View } from "react-native";
+
+export default function Index() {
+
+  const router = useRouter();
+
+  return (
+    <View className="flex-1 bg-primary">
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="#030014"
+      />
+      <Image source={images.bg} className="absolute w-full z-0"></Image>
+
+
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom:10}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        <View className="flex mt-5">
+          <SearchBar
+            onPress={()=>router.push("/search")}
+            placeholder="Search for a movie"
+          />
+
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+const search = () => {
+  return (
+    <View className='flex flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      
+    </View>
+  )
+}
+
+export default search
